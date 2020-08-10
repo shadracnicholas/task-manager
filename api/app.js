ea49c4c4235112ad0ef6a69895bea00b72c6f851
SHADRACK NICHOLAS <46599542+shadracnicholas@users.noreply.github.com>
2020-08-10 22:11:29 +0300

    Refactored routes

--- api/app.js
+++ api/app.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require("cors");
 
 const bodyparser = require("body-parser");
+const route = require("./routes/route");
 
 //Load Middleware
 app.use(cors());
@@ -10,39 +11,8 @@ app.use(bodyparser.json({ limit: 50000000, type: "application/json" }));
 
 const { mongoose } = require("./config/db");
 
-//Load in Mongoose models
-const { List, Task } = require("./models");
-
-// List Route Handlers
-app.get("/lists", (req, res) => {
-  //Return an array of all lists in JSON body
-  List.find({}).then((lists) => {
-    res.send(lists);
-  });
-});
-
-app.post("/lists", (req, res) => {
-  //Create a new list and return the new list document including the id
-
-  let title = req.body.title;
-
-  let newList = new List({
-    title,
-  });
-
-  newList.save().then((listDoc) => {
-    //Return full list document
-    res.send(listDoc);
-  });
-});
-
-app.patch("/list/:id", (req, res) => {
-  //Updates a specified list
-});
-
-app.delete("/list/:id", (req, res) => {
-  //Deletes a specified list from DB
-});
+// Load Endpoint Handlers
+app.use("/api", route);
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
--- api/routes/route.js
+++ api/routes/route.js
@@ -1,2 +1,58 @@
+const express = require("express");
+const router = express.Router();
+const { List, Task } = require("../models");
 
+//Get Lists API Endpoints
 
+//Return an array of all lists in JSON body
+router.get("/lists", (req, res) => {
+  List.find({}).then((lists) => {
+    res.send(lists);
+  });
+});
+
+//Create a new list and return the new list document including the id
+router.post("/lists", (req, res) => {
+  let title = req.body.title;
+
+  let newList = new List({
+    title,
+  });
+
+  newList.save().then((listDoc) => {
+    //Return full list document
+    res.send(listDoc);
+  });
+});
+
+//Updates a specified list
+router.patch("/lists/:id", (req, res) => {
+  List.findOneAndUpdate(
+    { _id: req.params.id },
+    {
+      $set: req.body,
+    }
+  ).then(() => {
+    res.sendStatus(200);
+  });
+});
+
+//Deletes a specified list from DB
+router.delete("/lists/:id", (req, res) => {
+  List.findByIdAndRemove({
+    _id: req.params.id,
+  }).then((removedListDoc) => {
+    res.send(removedListDoc);
+  });
+});
+
+//Get Tasks API Endpoints
+
+//Return All tasks of a specified list
+router.get("/lists/:listId/tasks", (req, res) => {
+  Task.find({
+    _listId: req.params.listId,
+  });
+});
+
+module.exports = router;
